d152236595fd275e19d3c3aa30c0d995b59a033f
Nata StereO8 <nataliepotapova30@gmail.com>
2021-04-19 22:19:11 +0300

    14-я практическиая

--- controllers/users.js
+++ controllers/users.js
@@ -1,4 +1,16 @@
 const User = require('../models/user');
+const bcrypt = require('bcryptjs'); // импортируем bcrypt
+
+exports.createUser = (req, res) => {
+  // хешируем пароль
+  bcrypt.hash(req.body.password, 10)
+    .then(hash => User.create({
+      email: req.body.email,
+      password: hash, // записываем хеш в базу
+    }))
+    .then((user) => res.send(user))
+    .catch((err) => res.status(400).send(err));
+};
 
 module.exports.createUser = (req, res) => {
   const { name, about, avatar } = req.body;
